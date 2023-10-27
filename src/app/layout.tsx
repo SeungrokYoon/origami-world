4228fa11c1be8efaaafbec972b5c0e14a8e32162
SeungrokYoon <sonicrok93@gmail.com>
2023-10-28 02:56:27 +0900

    config: Tailwind CSS 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "@/styles/globals.css";
 import RecoilRootProvider from "@/components/provider/RecoilRootProvider";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -18,7 +18,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <RecoilRootProvider>{children}</RecoilRootProvider>
+        <div>
+          <RecoilRootProvider>{children}</RecoilRootProvider>
+        </div>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,13 @@ import Text from "@/components/Text";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <Text />
         <FontButton />
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <button className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">
+          Button
+        </button>
       </div>
     </main>
   );
