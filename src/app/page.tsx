84e409914363270d2658c7213f900131392f50fc
SeungrokYoon <sonicrok93@gmail.com>
2023-11-01 00:20:04 +0900

    feat: add NavBar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,37 @@ import Text from "@/components/Text";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+    <main className="flex min-h-screen flex-col items-center justify-between pl-8 pr-8">
+      <div className="w-full max-w-5xl font-mono text-sm ">
+        <nav className="flex h-20 w-full justify-between bg-green-300">
+          <div>Logo</div>
+          <div className="flex">
+            <a
+              href="/menu1"
+              className="inline-flex w-fit items-center justify-center p-2 text-center hover:bg-slate-500 hover:text-white"
+            >
+              menu1
+            </a>
+            <a
+              href="/menu1"
+              className="inline-flex w-fit items-center justify-center p-2 text-center hover:bg-slate-500 hover:text-white"
+            >
+              menu1
+            </a>
+            <a
+              href="/menu1"
+              className="inline-flex w-fit items-center justify-center p-2 text-center hover:bg-slate-500 hover:text-white"
+            >
+              menu1
+            </a>
+            <a
+              href="/menu1"
+              className="inline-flex w-fit items-center justify-center p-2 text-center hover:bg-slate-500 hover:text-white"
+            >
+              menu1
+            </a>
+          </div>
+        </nav>
         <Text />
         <FontButton />
         <h1 className="text-3xl font-bold underline">Hello world!</h1>
