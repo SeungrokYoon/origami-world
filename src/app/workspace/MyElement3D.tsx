e8fcbfbcc24d8ae69a67fb0585eeec024f2baa64
SeungrokYoon <sonicrok93@gmail.com>
2023-11-03 02:03:02 +0900

    feat: add child mesh

--- src/app/workspace/MyElement3D.tsx
+++ src/app/workspace/MyElement3D.tsx
@@ -3,7 +3,7 @@
 import { OrbitControls } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
 import { RefObject, useRef } from "react";
-import { Mesh } from "three";
+import { MathUtils, Mesh } from "three";
 
 export default function MyElement3D() {
   const meshRef: RefObject<Mesh> = useRef<Mesh>(null);
@@ -20,9 +20,24 @@ export default function MyElement3D() {
       <axesHelper scale={10} />
       <OrbitControls />
       {/* 3d물체는 mesh */}
-      <mesh ref={meshRef} rotation={[0, 45 * (Math.PI / 180), 0]}>
+      <mesh
+        ref={meshRef}
+        position={[0, 2, 0]}
+        rotation={[0, 0, MathUtils.degToRad(20)]}
+        scale={[2, 1, 1]}
+      >
         <boxGeometry />
-        <meshStandardMaterial color="#e67e22" />
+        <meshStandardMaterial
+          color="#e67e22"
+          opacity={0.5}
+          transparent={true}
+        />
+        <axesHelper />
+        <mesh scale={[0.1, 0.1, 0.1]} position-y={2}>
+          <sphereGeometry />
+          <meshStandardMaterial color={"red"} />
+          <axesHelper scale={5} />
+        </mesh>
       </mesh>
     </>
   );
